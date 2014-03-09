51480324c6e544ab0194a4c411c8e3f06b6e7ecb
Aaron Jay <aaronjaylev@gmail.com>
2014-03-09 13:52:44 -0400

    Update config.php
    Added License

--- config.php
+++ config.php
@@ -1,5 +1,39 @@
 <?php
 
+/**
+* SageTask.com
+*
+* SageTask.com is a Website, API, and SDK for Creating and Managing simple tasks.
+*
+* This code is visible on http://www.sagetask.com.  It is a "one page" website
+* and uses JQuery, JQueryUI, Twitter Bootstrap to format the pages.  Connection
+* to the MySQL database is done through an API which can also be called directly.
+* I hope you enjoy viewing, using and learning from this code.  You may use it for 
+* your own projects if you give me credit by leaving this license notification 
+* in your files.  Happy Coding.  Aaron Jay
+*
+* @package SageTask
+* @author Aaron Jay Lev <aaronjaylev@gmail.com>
+* @copyright Copyright (c) 2014, Aaron Jay Lev
+* @link http://www.sagetask.com
+* @example http://www.sagetask.com/
+* @license http://www.apache.org/licenses/LICENSE-2.0
+*
+* Copyright 2014 Aaron Jay Lev
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 include_once "class.EvoSQL.php";
 
 $db_host = "localhost";
